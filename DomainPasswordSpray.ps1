7722e61c1c55df5ff0ae8ff09e4d999a1fb5db46
Rosie147 <100248757+Rosie147@users.noreply.github.com>
2022-04-01 12:57:24 +1000

    Fixes for ObservationWindow
    Updated calls to Get-ObservationWindow if DC is not PDC and added parameter to manually set set time between sprays to bypass if PDC cannot be queried

--- DomainPasswordSpray.ps1
+++ DomainPasswordSpray.ps1
@@ -120,10 +120,14 @@ function Invoke-DomainPasswordSpray{
      $Jitter=0,
 
      [Parameter(Position = 10, Mandatory = $false)]
+     [int]
+     $TimeBetweenSpray=0
+
+     [Parameter(Position = 11, Mandatory = $false)]
      [switch]
      $Quiet,
 
-     [Parameter(Position = 11, Mandatory = $false)]
+     [Parameter(Position = 12, Mandatory = $false)]
      [int]
      $Fudge=10
     )
@@ -196,7 +200,17 @@ function Invoke-DomainPasswordSpray{
         Write-Host -ForegroundColor Yellow "[*] WARNING - Be very careful not to lock out accounts with the password list option!"
     }
 
-    $observation_window = Get-ObservationWindow $CurrentDomain
+    # If you can't look up the observation window, you can specify this parameter to override how many
+    #   minutes between sprays.
+    if ($TimeBetweenSpray)
+    {
+        $observation_window = $TimeBetweenSpray
+    }
+    else
+    {
+        $objDeDomain = [ADSI] "LDAP://$($DomainObject.PDCRoleOwner)"
+        $observation_window = Get-ObservationWindow $objDeDomain
+    }
 
     Write-Host -ForegroundColor Yellow "[*] The domain password policy observation window is set to $observation_window minutes."
     Write-Host "[*] Setting a $observation_window minute wait in between sprays."
@@ -396,7 +410,7 @@ function Get-DomainUserList
         }
     }
 
-    $observation_window = Get-ObservationWindow $CurrentDomain
+    $observation_window = Get-ObservationWindow $objDeDomain
 
     # Generate a userlist from the domain
     # Selecting the lowest account lockout threshold in the domain to avoid
